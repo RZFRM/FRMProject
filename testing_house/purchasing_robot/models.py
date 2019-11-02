f795c7f0cf71fe38797d71848674fca23d1c1d12
D8330056253D5EC7BD7FA00B2B4CB11B <D8330056253D5EC7BD7FA00B2B4CB11B@i-search.com.cn>
2019-11-02 21:07:02 +0800

    'RDB数据库'

--- testing_house/purchasing_robot/models.py
+++ testing_house/purchasing_robot/models.py
@@ -1,78 +1,78 @@
 from django.db import models
 
 # Create your models here.
-#
-# # TODO   采购请购机器人数据表
-#
-# class purchase_apply_table(models.Model):
-#     id= models.AutoField(primary_key=True,verbose_name="id")
-#     gmt_create= models.DateTimeField(null=True, verbose_name='创建时间')
-#     gmt_modified = models.DateTimeField(null=True, verbose_name='修改时间')
-#     purchase_number = models.CharField(max_length=20, null=True, verbose_name='请购单编号')
-#     purchase_usesing = models.CharField(max_length=20, null=True, verbose_name='请购类型')
-#     goods_number = models.CharField(max_length=20, null=True, verbose_name='货物编号')
-#     recommended_unite_price = models.CharField(max_length=20, null=True, verbose_name='需求单价')
-#     specification = models.CharField(max_length=20, null=True, verbose_name='规格信号')
-#     goods_count = models.CharField(max_length=20, null=True, verbose_name='货物数量')
-#     recommended_price = models.CharField(max_length=20, null=True, verbose_name='需求价格')
-#     applicant = models.CharField(max_length=20, null=True, verbose_name='申请人')
-#     user_name = models.CharField(max_length=20, null=True, verbose_name='用户名字')
-#     business_type = models.CharField(max_length=50, null=True, verbose_name='业务类型')
-#     purchase_time = models.CharField(max_length=50, null=True, verbose_name='请购时间')
-#     application_depart = models.CharField(max_length=50, null=True, verbose_name='请购部门')
-#     recommended_date = models.CharField(max_length=10,null=True, verbose_name='需求日期')
-#     purchase_apply_status = models.CharField(max_length=20, null=True, verbose_name='请购状态')
-#     department_head = models.CharField(max_length=50, null=True, verbose_name='部门领导')
-#     company_head = models.CharField(max_length=50, null=True, verbose_name='公司领导')
-#     business_name = models.CharField(max_length=50, null=True, verbose_name='业务名称')
-#     procurement_type = models.CharField(max_length=50, null=True, verbose_name='采购类类型')
-#
-#
-#     def __str__(self):
-#         return self.purchase_usesing
-#
-#     class Meta:
-#         db_table = 'purchase_apply_table'
-#
+# #
+# # # TODO   采购请购机器人数据表
+
+class purchase_apply_table(models.Model):
+    id= models.AutoField(primary_key=True,verbose_name="id")
+    gmt_create= models.DateTimeField(null=True, verbose_name='创建时间')
+    gmt_modified = models.DateTimeField(null=True, verbose_name='修改时间')
+    purchase_number = models.CharField(max_length=20, null=True, verbose_name='请购单编号')
+    purchase_usesing = models.CharField(max_length=20, null=True, verbose_name='请购类型')
+    goods_number = models.CharField(max_length=20, null=True, verbose_name='货物编号')
+    recommended_unite_price = models.CharField(max_length=20, null=True, verbose_name='需求单价')
+    specification = models.CharField(max_length=20, null=True, verbose_name='规格信号')
+    goods_count = models.CharField(max_length=20, null=True, verbose_name='货物数量')
+    recommended_price = models.CharField(max_length=20, null=True, verbose_name='需求价格')
+    applicant = models.CharField(max_length=20, null=True, verbose_name='申请人')
+    user_name = models.CharField(max_length=20, null=True, verbose_name='用户名字')
+    business_type = models.CharField(max_length=50, null=True, verbose_name='业务类型')
+    purchase_time = models.CharField(max_length=50, null=True, verbose_name='请购时间')
+    application_depart = models.CharField(max_length=50, null=True, verbose_name='请购部门')
+    recommended_date = models.CharField(max_length=10,null=True, verbose_name='需求日期')
+    purchase_apply_status = models.CharField(max_length=20, null=True, verbose_name='请购状态')
+    department_head = models.CharField(max_length=50, null=True, verbose_name='部门领导')
+    company_head = models.CharField(max_length=50, null=True, verbose_name='公司领导')
+    business_name = models.CharField(max_length=50, null=True, verbose_name='业务名称')
+    procurement_type = models.CharField(max_length=50, null=True, verbose_name='采购类类型')
+
+
+    def __str__(self):
+        return self.purchase_usesing
+
+    class Meta:
+        db_table = 'purchase_apply_table'
+
 #
 
 
-#
-# # TODO   采购合同机器人数据表
-#
-# class purchase_contract_table(models.Model):
-#     id  = models.AutoField(primary_key=True,verbose_name="id")
-#     user_name = models.CharField(max_length=20, null=True, verbose_name='用户名')
-#     gmt_create = models.DateTimeField(null=True, verbose_name='创建时间')
-#     gmt_modified = models.DateTimeField(null=True, verbose_name='修改时间')
-#     purchase_number = models.CharField(max_length=20, null=True, verbose_name='请购单编号')
-#     contract_number  = models.CharField(max_length=20, null=True, verbose_name='采购合同编号')
-#     supplier_name = models.CharField(max_length=20, null=True, verbose_name='供应商')
-#     tax_rate   = models.CharField(max_length=20, null=True, verbose_name='税率')
-#     free_tax_unit_price  = models.CharField(max_length=20, null=True, verbose_name='免税单价')
-#     count = models.CharField(max_length=20, null=True, verbose_name='数量')
-#     summary_price = models.CharField(max_length=20, null=True, verbose_name='总价')
-#     applicant = models.CharField(max_length=20, null=True, verbose_name='申请人')
-#     application_sector = models.CharField(max_length=20, null=True, verbose_name='申请部门')
-#     application_date = models.CharField(max_length=20, null=True, verbose_name='申请日期')
-#     department_head = models.CharField(max_length=20, null=True, verbose_name='部门领导')
-#     company_head = models.CharField(max_length=20, null=True, verbose_name='公司领导')
-#     contract_apply_status = models.CharField(max_length=20, null=True, verbose_name='合同机器人状态')
-#     business_name = models.CharField(max_length=50, null=True, verbose_name='业务名称')
-#     business_type = models.CharField(max_length=20, null=True, verbose_name='业务类型')
-#     purchase_type = models.CharField(max_length=20, null=True, verbose_name='采购类型')
-#     planne_arrive_date = models.CharField(max_length=20, null=True, verbose_name='预计到达日期')
-#     demand_date = models.CharField(max_length=50, null=True, verbose_name='需求日期')
-#
-#
-#     def __str__(self):
-#         return self.contract_number
-#
-#     class Meta:
-#         db_table = 'purchase_contract_table'
-#
-#
+# #
+# # # TODO   采购合同机器人数据表
+# #
+class purchase_contract_table(models.Model):
+    id  = models.AutoField(primary_key=True,verbose_name="id")
+    user_name = models.CharField(max_length=20, null=True, verbose_name='用户名')
+    gmt_create = models.DateTimeField(null=True, verbose_name='创建时间')
+    gmt_modified = models.DateTimeField(null=True, verbose_name='修改时间')
+    purchase_number = models.CharField(max_length=20, null=True, verbose_name='请购单编号')
+    contract_number  = models.CharField(max_length=20, null=True, verbose_name='采购合同编号')
+    supplier_name = models.CharField(max_length=20, null=True, verbose_name='供应商')
+    tax_rate   = models.CharField(max_length=20, null=True, verbose_name='税率')
+    free_tax_unit_price  = models.CharField(max_length=20, null=True, verbose_name='免税单价')
+    count = models.CharField(max_length=20, null=True, verbose_name='数量')
+    summary_price = models.CharField(max_length=20, null=True, verbose_name='总价')
+    applicant = models.CharField(max_length=20, null=True, verbose_name='申请人')
+    application_sector = models.CharField(max_length=20, null=True, verbose_name='申请部门')
+    application_date = models.CharField(max_length=20, null=True, verbose_name='申请日期')
+    department_head = models.CharField(max_length=20, null=True, verbose_name='部门领导')
+    company_head = models.CharField(max_length=20, null=True, verbose_name='公司领导')
+    contract_apply_status = models.CharField(max_length=20, null=True, verbose_name='合同机器人状态')
+    business_name = models.CharField(max_length=50, null=True, verbose_name='业务名称')
+    business_type = models.CharField(max_length=20, null=True, verbose_name='业务类型')
+    purchase_type = models.CharField(max_length=20, null=True, verbose_name='采购类型')
+    planne_arrive_date = models.CharField(max_length=20, null=True, verbose_name='预计到达日期')
+    demand_date = models.CharField(max_length=50, null=True, verbose_name='需求日期')
 
+
+    def __str__(self):
+        return self.contract_number
+
+    class Meta:
+        db_table = 'purchase_contract_table'
+
+#
+#
 
 
 # TODO   采购入库机器人数据表
@@ -81,6 +81,7 @@ class purchase_warehousing_table(models.Model):
     id= models.AutoField(primary_key=True,verbose_name="id")
     gmt_create= models.DateTimeField(null=True, verbose_name='创建时间')
     gmt_modified = models.DateTimeField(null=True, verbose_name='修改时间')
+    approval_date  =  models.DateTimeField(null=True, verbose_name='审批日期')
     purchase_number = models.CharField(max_length=20, null=True, verbose_name='请购单编号')
     contract_number = models.CharField(max_length=20, null=True, verbose_name='合同编号')
     warehouse_number = models.CharField(max_length=20, null=True, verbose_name='仓库编号')
@@ -96,9 +97,9 @@ class purchase_warehousing_table(models.Model):
 
 
 
-
-
-# TODO   采购发票机器人数据表
+#
+#
+# # TODO   采购发票机器人数据表
 
 class purchase_invoice_table(models.Model):
     id  = models.AutoField(primary_key=True,verbose_name="id")
@@ -109,8 +110,8 @@ class purchase_invoice_table(models.Model):
     contract_number  = models.CharField(max_length=20, null=True, verbose_name='采购合同编号')
     incoive_number = models.CharField(max_length=20, null= True, verbose_name='发票号码')
     reimbursement_type = models.CharField(max_length=20, null= True, verbose_name='报销类别')
-    reimbursement_money = models.CharField(max_length=20, null= True, verbose_name='发票号码')
-    money_details = models.CharField(max_length=20, null= True, verbose_name='价格')
+    reimbursement_money = models.CharField(max_length=20, null= True, verbose_name='报销金额')
+    money_details = models.CharField(max_length=20, null= True, verbose_name='费用明细')
     supplier_name = models.CharField(max_length=20, null= True, verbose_name='供应商信息')
     application_date = models.CharField(max_length=20, null=True, verbose_name='申请日期')
     application = models.CharField(max_length=20, null=True, verbose_name='申请人')
@@ -125,11 +126,11 @@ class purchase_invoice_table(models.Model):
 
     class Meta:
         db_table = 'purchase_invoice_table'
-
-
-
-# TODO   采购付款机器人数据表
-
+#
+#
+#
+# # TODO   采购付款机器人数据表
+#
 class purchase_payment_table(models.Model):
     id  = models.AutoField(primary_key=True,verbose_name="id")
     user_name = models.CharField(max_length=20, null=True, verbose_name='用户名')
