728701063786c3660bd9797ba24e1c5501fe9ab4
D8330056253D5EC7BD7FA00B2B4CB11B <D8330056253D5EC7BD7FA00B2B4CB11B@i-search.com.cn>
2019-11-02 19:43:26 +0800

    'purchase.models更新'

--- testing_house/purchasing_robot/views.py
+++ testing_house/purchasing_robot/views.py
@@ -154,7 +154,7 @@ def purchaes_requisitions_create(request):
     #
     # purchase_number = 'CG0000007'
     # print(locals())
-    return  render(request, 'purchaes_storage_6.html')
+    return  render(request, 'purchaes_payment_10.html')
     return render(request, 'purchaes_requisitions_2.html', locals())
 
 
