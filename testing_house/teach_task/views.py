4773139952d79da6ffbc9890c907bb16132c7f40
baishanwangqinghong <490960340@qq.com>
2019-11-06 10:23:12 +0800

    上传

--- testing_house/teach_task/views.py
+++ testing_house/teach_task/views.py
@@ -109,21 +109,21 @@ class Task(View):
             return JsonResponse({"result": "fail", "msg": "系统错误，请重试"})
 
         if admin_type:
-            task_root = [{"name": "实训任务", "url": ""}, {"name": "实训任务卡", "url": ""}, {"name": "教学管理", "url": ""},
+            task_root = [{"name": "实训任务", "url": ""}, {"name": "教学管理", "url": ""},
                          {"name": "学生管理", "url": "/student_jump"}, {"name": "教师管理", "url": "/teacher_jump"},
                          {"name": "班级管理", "url": "/class_jump"}, {"name": "专业管理", "url": "/major_jump"},
                          {"name": "教务管理", "url": "/teach_jump"}, {"name": "学校管理", "url": "/school_jump"},
                          {"name": "课程管理", "url": ""}, {"name": "案例管理", "url": ""}]
 
-            task_edu = [{"name": "实训任务", "url": ""}, {"name": "实训任务卡", "url": ""}, {"name": "教学管理", "url": ""},
+            task_edu = [{"name": "实训任务", "url": ""},  {"name": "教学管理", "url": ""},
                         {"name": "学生管理", "url": "/student_jump"}, {"name": "教师管理", "url": "/teacher_jump"},
                         {"name": "班级管理", "url": "/class_jump"}, {"name": "专业管理", "url": "/major_jump"},
                         {"name": "教务管理", "url": "/teach_jump"}]
 
-            task_teach = [{"name": "实训任务", "url": ""}, {"name": "实训任务卡", "url": ""}, {"name": "教学管理", "url": ""},
+            task_teach = [{"name": "实训任务", "url": ""}, {"name": "教学管理", "url": ""},
                           {"name": "学生管理", "url": "/student_jump"}]
 
-            task_student = [{"name": "实训任务", "url": ""}, {"name": "实训任务卡", "url": ""}]
+            task_student = [{"name": "实训任务", "url": ""}]
 
             if admin_type[0] == '1':
                 return JsonResponse({"result": task_root})
