4762cf8a736e4aa451ca0755d7a069899fdffddb
D8330056253D5EC7BD7FA00B2B4CB11B <D8330056253D5EC7BD7FA00B2B4CB11B@i-search.com.cn>
2019-11-15 11:26:01 +0800

    'update'

--- testing_house/purchasing_robot/urls.py
+++ testing_house/purchasing_robot/urls.py
@@ -21,21 +21,36 @@ from django.conf.urls import url
 urlpatterns = [
 
     #  TODO  数据页面
-    url('pruchasing_robot_base_data', views.pruchasing_robot_base_data, name='采购机器人基础配置页面'),
-    url('purchasing_chose_models',views.set_purchasing_chose_models,name = '采购选择案例'),
-    url('purchaes_requisitions_create_data', views.purchaes_requisitions_create_data,
-        name='purchaes_requisitions_create'),
+
     # TODO　　数据接口
+    url('purchasing_models_data', views.set_purchasing_chose_models, name='采购选择案例数据'),
     url('purchase_apply_data', views.set_apply_data, name='采购请购单数据'),
     url('purchase_contract_data', views.set_contract_data, name='采购合同单数据'),
     url('purchase_warehousing_data', views.set_warehousing_data, name='采购入库单数据'),
     url('purchase_invoice_data', views.set_invoice_data, name='采购报销单数据'),
     url('purchase_payment_data', views.set_payment_data, name='采购报销单数据'),
 
+    url('purchase_robot_buession_info', views.set_purchase_robot_buession_info, name='采购机器人业务数据'),
+    url('purchase_robot_jobs_info', views.set_purchase_robot_jobs_info, name='采购机器任务务数据'),
+    url('pruchasing_robot_base_data', views.pruchasing_robot_base_data, name='采购机器人基础配置页面'),
+
+    url('purchaes_requisitions_create_data', views.purchaes_requisitions_create_data,name='purchaes_requisitions_create'),
+
+    url('purchase_view_information_data', views.set_view_information_data, name='查看数据'),
+
     #  TODO　　页面接口
+
     url('purchasing_created', views.purchasing_created, name='选择案例编号'),
     url('purchaes_requisitions_create', views.purchaes_requisitions_create, name='物资请购'),
+    url('purchaes_contract_create', views.purchaes_contract_create, name='采购合同页面接口'),
+    url('purchaes_storage_create', views.purchaes_storage_create, name='物资入库页面接口'),
+    url('purchaes_reimburse_create', views.purchaes_reimburse_create, name='报销申请页面接口'),
+    url('purchaes_payment_create', views.purchaes_payment_create, name='付款申请页面接口'),
+    url('pruchasing_robot_base', views.pruchasing_robot_base, name='采购机器人基础配置页面'),
 
+    url('purchasing_robot_business_manager', views.pruchasing_robot_business, name='采购机器人业务管理页面'),
+    url('purchasing_robot_jobs_manager', views.pruchasing_robot_jobs, name='采购机器人任务管理页面'),
+    url('purchase_view_information', views.set_view_information, name='查看页面'),
     # url('purchasing_created_data', views.purchasing_created_data, name='purchasing_created'),
     # url('purchaes_order_create_data', views.set_purchaes_order_create_data, name='purchaes_requisitions_create'),
     # url('purchaes_storage_create_data', views.set_purchaes_storage_create_data, name='purchaes_requisitions_create'),
@@ -51,30 +66,18 @@ urlpatterns = [
 
     # url('purchaes_requisitions_determine', views.purchaes_requisitions_determine,
     #     name='purchaes_requisitions_determine'),
-    url('purchaes_order_create', views.purchaes_order_create, name='purchaes_order_create'),
     # url('purchaes_order_determine', views.purchaes_order_determine, name='purchaes_order_determine'),
 
-    url('purchaes_storage_create', views.purchaes_storage_create, name='purchaes_storage_create'),
     # url('purchaes_storage_determine', views.purchaes_storage_determine, name='purchaes_storage_determine'),
 
-    url('purchaes_reimburse_create', views.purchaes_reimburse_create, name='purchaes_reimburse_create'),
     # url('purchaes_reimburse_determine', views.purchaes_reimburse_determine, name='purchaes_reimburse_determine'),
 
-    url('purchaes_payment_create', views.purchaes_payment_create, name='purchaes_payment_create'),
     # url('purchaes_payment_determine', views.purchaes_payment_determine, name='purchaes_payment_determine'),
 
-    url('purchaes_business_data_display', views.purchaes_business_data_display, name='purchaes_business_data_display'),
+    # url('purchaes_business_data_display', views.purchaes_business_data_display, name='purchaes_business_data_display'),
 
-    url('pruchasing_robot_base', views.pruchasing_robot_base, name='set_robot'),
 
-    url('purchasing_robot_business_manager', views.pruchasing_robot_business, name='set_robot'),
-    url('purchasing_robot_jobs_manager', views.pruchasing_robot_jobs, name='set_prokject'),
 
-    url('purchase_robot_buession_info', views.set_purchase_robot_buession_info, name='set_purchase_robot_jobs_info'),
-    url('purchase_robot_jobs_info', views.set_purchase_robot_jobs_info, name='set_purchase_robot_jobs_info'),
-
-    url('purchase_view_information_data', views.set_view_information_data, name='查看数据'),
-    url('purchase_view_information', views.set_view_information, name='查看页面'),
 
 
 
--- testing_house/testing_house/urls.py
+++ testing_house/testing_house/urls.py
@@ -38,7 +38,6 @@ urlpatterns = [
     path('', include('corporate_tax_quarterly_report_robot.urls')),
     path('', include('vat_return_robot.urls')),
 
-
     url('^fixed_assets/', include('fixed_assets_robot.urls')),
 
     path('', include('sales_robot.urls')),
